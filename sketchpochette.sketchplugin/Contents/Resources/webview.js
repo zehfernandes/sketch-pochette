976c6886ffc7bedc0c20bfa8643b32d1c7715590
zehfernandes <ozehfernandes@gmail.com>
2018-11-12 10:50:01 -0200

    new version

--- sketchpochette.sketchplugin/Contents/Resources/webview.js
+++ sketchpochette.sketchplugin/Contents/Resources/webview.js
@@ -1,5 +1,149 @@
-!function(e){var t={};function n(o){if(t[o])return t[o].exports;var u=t[o]={i:o,l:!1,exports:{}};return e[o].call(u.exports,u,u.exports,n),u.l=!0,u.exports}n.m=e,n.c=t,n.d=function(e,t,o){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:o})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(n.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var u in e)n.d(o,u,function(t){return e[t]}.bind(null,u));return o},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s="./resources/webview.js")}({"./resources/webview.js":
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./resources/webview.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./resources/webview.js":
 /*!******************************!*\
   !*** ./resources/webview.js ***!
   \******************************/
-/*! no static exports found */function(e,t){document.addEventListener("contextmenu",function(e){e.preventDefault()}),window.postMessage("loadSelectElements","Called from the webview"),window.writeNotes=function(e){var t=e[0].x,n=e[0].y,o=e[0].width,u=e[0].height;e.forEach(function(e){t=t===e.x?t:"Multiple",n=n===e.y?n:"Multiple",o=o===e.width?o:"Multiple",u=u===e.height?u:"Multiple"}),document.getElementById("posx").value=t,document.getElementById("posy").value=n,document.getElementById("width").value=o,document.getElementById("height").value=u},window.unload=function(){document.getElementById("posx").blur(),document.getElementById("posy").blur(),document.getElementById("width").blur(),document.getElementById("height").blur()},document.getElementById("posx").addEventListener("change",function(e){window.postMessage("updateElements","x",e.target.value)}),document.getElementById("posy").addEventListener("change",function(e){window.postMessage("updateElements","y",e.target.value)}),document.getElementById("width").addEventListener("change",function(e){window.postMessage("updateElements","width",e.target.value)}),document.getElementById("height").addEventListener("change",function(e){window.postMessage("updateElements","height",e.target.value)})}});
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// Disable the context menu to have a more native feel
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
+window.postMessage("loadSelectElements", "Called from the webview"); //Less Hardcore :(
+
+window.writeNotes = function (content) {
+  var xValue = content[0].x;
+  var yValue = content[0].y;
+  var widthValue = content[0].width;
+  var heightValue = content[0].height;
+  content.forEach(function (item) {
+    xValue = xValue === item.x ? xValue : "Multiple";
+    yValue = yValue === item.y ? yValue : "Multiple";
+    widthValue = widthValue === item.width ? widthValue : "Multiple";
+    heightValue = heightValue === item.height ? heightValue : "Multiple";
+  });
+  document.getElementById("posx").value = xValue;
+  document.getElementById("posy").value = yValue;
+  document.getElementById("width").value = widthValue;
+  document.getElementById("height").value = heightValue;
+};
+
+window.unload = function () {
+  document.getElementById("posx").blur();
+  document.getElementById("posy").blur();
+  document.getElementById("width").blur();
+  document.getElementById("height").blur();
+};
+
+function isNumberKey(evt) {
+  var charCode = evt.which ? evt.which : event.keyCode;
+  if (charCode > 31 && charCode != 46 && (charCode < 48 || charCode > 57)) return false;
+  return true;
+} // Listeners
+
+
+document.getElementById("posx").addEventListener("change", function (e) {
+  window.postMessage("updateElements", "x", e.target.value);
+});
+document.getElementById("posy").addEventListener("change", function (e) {
+  window.postMessage("updateElements", "y", e.target.value);
+});
+document.getElementById("width").addEventListener("change", function (e) {
+  window.postMessage("updateElements", "width", e.target.value);
+});
+document.getElementById("height").addEventListener("change", function (e) {
+  window.postMessage("updateElements", "height", e.target.value);
+});
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=webview.js.map
