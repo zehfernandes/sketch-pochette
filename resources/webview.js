c083184ed9ad95e17554e43916190cebf575cfb6
zehfernandes <ozehfernandes@gmail.com>
2019-01-11 20:09:41 -0200

    improve ux of the field

--- resources/webview.js
+++ resources/webview.js
@@ -1,14 +1,14 @@
 /* global document, window */
 
 // Disable the context menu to have a more native feel
-document.addEventListener('contextmenu', e => {
+document.addEventListener('contextmenu', (e) => {
   e.preventDefault()
 })
 
-window.postMessage('loadSelectElements')
+window.postMessage('loadSelectElements', 'Called from the webview')
 
 // Less Hardcore :(
-window.writeNotes = content => {
+window.writeNotes = function(content) {
   let xValue = content[0].x
   let yValue = content[0].y
   let widthValue = content[0].width
@@ -26,19 +26,30 @@ window.writeNotes = content => {
   document.getElementById('height').value = heightValue
 }
 
-window.unload = () => {
+window.unload = function() {
   document.getElementById('posx').blur()
   document.getElementById('posy').blur()
   document.getElementById('width').blur()
   document.getElementById('height').blur()
 }
 
-// function isNumberKey(evt) {
-//   let charCode = evt.which ? evt.which : event.keyCode
-//   if (charCode > 31 && (charCode != 46 && (charCode < 48 || charCode > 57)))
-//     return false
-//   return true
-// }
+function arrowKeys(e, prop) {
+  const keyCode = e.keyCode || e.which
+  if ((keyCode == 38 || keyCode == 40) && e.target.value != 'Multiple') {
+    let value = Number(e.target.value)
+    const mod = {
+      38: 1,
+      40: -1,
+    }
+
+    value += mod[keyCode]
+    if (e.shiftKey) value = mod[keyCode] > 0 ? value + 9 : value - 9
+    window.postMessage('updateElements', prop, value)
+
+    e.preventDefault()
+    return false
+  }
+}
 
 // Listeners
 document.getElementById('posx').addEventListener('change', e => {
@@ -55,4 +66,20 @@ document.getElementById('width').addEventListener('change', e => {
 
 document.getElementById('height').addEventListener('change', e => {
   window.postMessage('updateElements', 'height', e.target.value)
+})
+
+document.getElementById('posx').addEventListener('keydown', e => {
+  arrowKeys(e, 'x')
+})
+
+document.getElementById('posy').addEventListener('keydown', e => {
+  arrowKeys(e, 'y')
+})
+
+document.getElementById('width').addEventListener('keydown', e => {
+  arrowKeys(e, 'width')
+})
+
+document.getElementById('height').addEventListener('keydown', e => {
+  arrowKeys(e, 'height')
 })
